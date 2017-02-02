89db44e26a5fc43d166a39e6f5f968fb42166ec7
melux <meluxford@meluxford.com>
2017-02-02 10:59:46 +0100

    Add feedback sliding panel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,19 @@ $(document).ready(function(e) {
 		$('.logo-default').css('display', 'none');
 		$('.logo-tanzania').css('display', 'block');
 	}
+
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').addClass('is-visible');
+	});
+	$('.cd-panel-close').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').removeClass('is-visible');
+	});
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+			$('.cd-panel').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
 })
